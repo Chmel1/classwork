bf7654e70232e1248178fd5034ec645bc17f86ec
Chmel1 <145644543+Chmel1@users.noreply.github.com>
2024-09-09 12:19:58 +0300

    Add files via upload
    ещкере

---
+++ database/database/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [User_id] INT NOT NULL, 
+    [Email] NCHAR(52) NULL, 
+    [Password] NCHAR(52) NULL, 
+    [First_Name] NCHAR(52) NULL, 
+    [Last_Name] NCHAR(52) NULL, 
+    [Role_Id] INT NULL Foreign key references [Role]([Role_id]),
+    CONSTRAINT [PK_User] PRIMARY KEY ([User_id]),
+)
